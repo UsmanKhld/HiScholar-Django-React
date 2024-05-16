94839fd771d234dd89e6c241356270a050a3de97
Izaan Khalid <izaan.khalid@outlook.com>
2024-05-16 18:28:58 -0500

    Finalized serializers

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,5 +11,5 @@ urlpatterns = [
     path('api/user/register/', CreateUserView.as_view(), name="register"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('api-auth/', include("rest_framework.urls")),
+    path('api-auth/', include("rest_framework.urls")),
 ]
