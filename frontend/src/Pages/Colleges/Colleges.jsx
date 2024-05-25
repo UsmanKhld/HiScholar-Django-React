9923771066967bd15f461b534e4c56ad7f27e2b0
Izaan Khalid <izaan.khalid@outlook.com>
2024-05-25 03:56:39 -0500

    College Search Changes

--- frontend/src/Pages/Colleges/Colleges.jsx
+++ frontend/src/Pages/Colleges/Colleges.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Navbar } from '../../Components/index'
 import './Colleges.css'
 
@@ -7,17 +9,23 @@ export const Colleges = () => {
     <div>
       <Navbar />
       <main>
-        <div className="topnav grid grid-cols-2">
-          <div className="underline bold">
+        <div className="topnav grid grid-cols-3 border-solid border-b-4 border-black pb-2 ">
+          <div className="bold justify-self-end pr-3">
             <h1>
               College Search
             </h1>
           </div>
           <div className="pl-10">
-            <input type="text" placeholder="Search by college name" className="h-full w-full pl-5 rounded-lg"></input>
+            <button className="search_input border-solid border-4 border-black rounded-full w-full h-18 bg-white">
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute fa-2x" />
+              <span className="pl-12 text-xl">Search by college name</span>
+            </button>
+          </div>
+          <div className="pl-20 self-center">
+            <FontAwesomeIcon icon={faThumbsUp} className="fa-3x mr-3" />
           </div>
         </div>
-      </main>
+      </main >
     </div >
   )
 }
