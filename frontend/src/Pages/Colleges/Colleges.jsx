dd9ddd67ca70a3a94511e1c69919ed48d0674149
Izaan Khalid <izaan.khalid@outlook.com>
2024-05-29 19:43:05 -0500

    Adding npm-fetch library and changes to Colleges

--- frontend/src/Pages/Colleges/Colleges.jsx
+++ frontend/src/Pages/Colleges/Colleges.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbsUp, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import { Navbar } from '../../Components/index'
 import './Colleges.css'
 
@@ -10,23 +11,39 @@ export const Colleges = () => {
       <Navbar />
       <main>
         <div className="topnav grid grid-cols-3 border-solid border-b-4 border-black pb-2 ">
-          <div className="bold justify-self-end pr-3">
-            <h1>
+          <div className="bold pr-3">
+            <div className="text-4xl text-blue-900">
               College Search
-            </h1>
+            </div>
           </div>
-          <div className="pl-10">
-            <button className="search_input border-solid border-4 border-black rounded-full w-full h-18 bg-white">
-              <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute fa-2x" />
-              <span className="pl-12 text-xl">Search by college name</span>
+          <div className="pl-10 pb-2 truncate">
+            <button className="search_input border-solid border-4 border-black rounded-full w-full h-13 bg-white">
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute fa-lg pt-0.5" />
+              <span className="pl-10 text-lge">Search by college name</span>
             </button>
           </div>
-          <div className="pl-20 self-center">
+          <div className="pl-20 self-center justify-self-center">
             <FontAwesomeIcon icon={faThumbsUp} className="fa-3x mr-3" />
           </div>
         </div>
-      </main >
-    </div >
+        <div className="content grid grid-rows-2">
+          <div className="colleges p-5 pl-10">
+            <div className="text-xl text-black">1000 Colleges Found</div>
+          </div>
+          <div className="college-card w-11/12 justify-self-center">
+            <button className="grid grid-cols-3 w-full">
+              <div className="justify-self-center self-center">
+                <FontAwesomeIcon icon={faThumbsUp} className="fa-2x mr-3" />
+              </div>
+              <div className="p-1 col-span-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              </div>
+            </button>
+
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
