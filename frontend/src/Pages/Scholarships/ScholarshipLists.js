3274d56e17b2b6486bfabb832fda263514ee57e8
Usman Khalid <usmankhld602@gmail.com>
2024-06-27 14:08:08 -0500

    list

--- frontend/src/Pages/Scholarships/ScholarshipLists.js
+++ frontend/src/Pages/Scholarships/ScholarshipLists.js
@@ -77,4 +77,92 @@ export const ScLists = [
         apply: "https://www.cia.gov/careers/jobs/undergraduate-scholarship-program/",
         description: "The Central Intelligence Agency Undergraduate Scholarship provides scholarship/loans and work experience to high school seniors and college sophomores who would be interested in working for the Central Intelligence Agency (CIA) after graduation from college."
     },
+    {
+        id: 8,
+        title: "Ahead Together",
+        due: "06/30/2024",
+        amount: 2000,
+        gpa: 2.8,
+        sat: "1000",
+        act: "26",
+        apply: "https://www.regiafoundation.org/scholarship",
+        description: "The Regia Foundation is an organization committed to providing scholarships to deserving students. Our mission is to empower the next generation of leaders by removing financial barriers to education, allowing them to achieve their full potential."
+    },
+    {
+        id: 9,
+        title: "Alexander and Alice C. Jarvis Memorial Scholarship",
+        due: "06/10/2024",
+        amount: 7000,
+        gpa: 3.0,
+        sat: "1000",
+        act: "24",
+        apply: "http://manchesterscholarship.org/",
+        description: "To support Manchester students who are pursuing four-year degrees and have received MSF Traditional Scholarships."
+    },
+    {
+        id: 10,
+        title: "Betty Rendel Scholarships",
+        due: "07/1/2024",
+        amount: 1000,
+        gpa: 2.5,
+        sat: "900",
+        act: "20",
+        apply: "http://www.nfrw.org/",
+        description: "Applicant must submit a completed application form together with three letters of recommendation (include contact numbers of the authors); an official copy of recent college transcript; a 1-page typed essay stating why the applicant deserves the scholarship; another 1-page typed essay on career goals; a photograph (optional); and State Federation President Certification. Application form is available online and must be submitted to applicant's State Federation President"
+    },
+    {
+        id: 11,
+        title: "Bill Dickey Scholarship Association Scholarship",
+        due: "07/19/2024",
+        amount: 14000,
+        gpa: 2.5,
+        sat: "1100",
+        act: "24",
+        apply: "http://billdickey.org/",
+        description: "Applications can be submitted online; must provide official transcript; essay must be a minimum of 500 words and it must be typed and double-spaced, no exceptions; additional pages to application; two personal reference forms should be completed by your high school principal, guidance counselor or other professional(s) who are knowledgeable of your: (a) academic record, (b) potential for successful college level academic immersion, (c) history of active participation in golf and (d) financial need; all uploaded files must be smaller than 3MB each; essay (uploaded below as doc or pdf); one photo head shot in high resolution (at least 1200 pixels wide and uploaded below as jpg, gif, png or pdf)."
+    },
+    {
+        id: 12,
+        title: "Bryan A. Champion Memorial Scholarship",
+        due: "07/01/2024",
+        amount: 1000,
+        gpa: 2.7,
+        sat: "800",
+        act: "20",
+        apply: "http://www.hriresearch.org/",
+        description: "Applications must be submitted through the online application system; general contact information, statement of financial need, personal essay, resume, transcripts, and two letters of recommendation."
+    },
+    {
+        id: 13,
+        title: "A.O. Putnam Memorial Scholarship",
+        due: "11/15/2024",
+        amount: 2500,
+        gpa: 3.4,
+        sat: "1150",
+        act: "22",
+        apply: "http://www.iise.org/",
+        description: "Applicants must be nominated by IE department heads; nomination forms are available online. Qualified nominees will submit an application package that includes: Submissions Form, transcripts (may be unofficial), 3 Reference Forms, optional letter of recommendations, and other specified criteria."
+    },
+    {
+        id: 14,
+        title: "LIFE Lessons Scholarship Program",
+        due: "08/01/2024",
+        amount: 200000,
+        gpa: 2.0,
+        sat: "800",
+        act: "20",
+        apply: "http://www.lifehappens.org/",
+        description: "Applicants must be complete and submit the online application form; an essay of no more than 500 words or video entry of no more than 3 minutes; must explain how the lack of adequate life insurance coverage (or no coverage at all) impacted your family’s financial situation; online entries must be received; Limit one (1) entry per entrant; entry must be submitted in English; entry must be 100% original and not contain third party materials subject to copyright including but not limited to music, video clips, artwork and photos."
+    },
+    {
+        id: 15,
+        title: "USHJA Foundation Hamel Scholarship for Further Education",
+        due: "Annual",
+        amount: 25000,
+        gpa: 3.0,
+        sat: "1000",
+        act: "30",
+        apply: "http://www.ushja.org/",
+        description: "To encourage further education among equestrians. Applicant must submit application form."
+    },
 ]
